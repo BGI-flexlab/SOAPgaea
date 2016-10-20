35f8ac150fe8cd48432f983fba483221dc481b05
huangzhibo <huangzhibo@genomics.cn>
2016-10-20 22:02:57 +0800

    modify

--- src/main/java/org/bgi/flexlab/gaea/data/structure/vcf/index/VCFLoader.java
+++ src/main/java/org/bgi/flexlab/gaea/data/structure/vcf/index/VCFLoader.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.util.LineReader;
