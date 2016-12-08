d69f8c55df4fb03d5672ab73a3cb0a0f00338c84
lishengkang <lishengkang@genomics.cn>
2016-12-08 17:06:53 +0800

    vcf local loader

--- src/main/java/org/bgi/flexlab/gaea/data/structure/vcf/VCFLocalLoader.java
+++ src/main/java/org/bgi/flexlab/gaea/data/structure/vcf/VCFLocalLoader.java
@@ -1,13 +1,8 @@
 package org.bgi.flexlab.gaea.data.structure.vcf;
 
 import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -16,32 +11,24 @@ import java.util.Iterator;
 import java.util.Set;
 import java.util.zip.GZIPInputStream;
 
-import org.bgi.flexlab.gaea.data.structure.reference.index.vcfIndex;
-import org.bgi.flexlab.gaea.data.structure.vcf.index.IndexCreator;
+import org.bgi.flexlab.gaea.data.structure.reference.index.VcfIndex;
 import org.tukaani.xz.SeekableFileInputStream;
 
-import htsjdk.samtools.seekablestream.SeekableStream;
-import htsjdk.samtools.util.CloseableIterator;
 import htsjdk.tribble.FeatureCodecHeader;
 import htsjdk.tribble.TribbleException;
-import htsjdk.tribble.index.Index;
-import htsjdk.tribble.index.IndexFactory;
-import htsjdk.tribble.index.tabix.TabixFormat;
 import htsjdk.tribble.readers.AsciiLineReader;
 import htsjdk.tribble.readers.AsciiLineReaderIterator;
 import htsjdk.tribble.readers.PositionalBufferedStream;
-import htsjdk.tribble.util.LittleEndianOutputStream;
 import htsjdk.tribble.util.ParsingUtils;
 import htsjdk.variant.variantcontext.VariantContext;
 import htsjdk.variant.vcf.VCFCodec;
-import htsjdk.variant.vcf.VCFFileReader;
 import htsjdk.variant.vcf.VCFHeader;
 
 public class VCFLocalLoader {
 	
 	public static final Set<String> BLOCK_COMPRESSED_EXTENSIONS = Collections.unmodifiableSet(new HashSet<String>(Arrays.asList(".gz", ".gzip", ".bgz", ".bgzf")));
 		
-	private String input, idxFile;
+	private String input;
 		
 	private SeekableFileInputStream seekableStream = null; 
 	
@@ -51,24 +38,12 @@ public class VCFLocalLoader {
 	
 	private long pos;
 	
-	public VCFLocalLoader(String refPath, String dbSNP) throws IOException {
+	public VCFLocalLoader(String dbSNP) throws IOException {
 		codec = new VCFCodec();
 		input = dbSNP;
 		readHeader();
 		seekableStream = new SeekableFileInputStream(input);
-		idxFile = format(dbSNP);
-		if(!(new File(idxFile)).exists()) {
-			vcfIndex index = new vcfIndex();
-			index.buildIndex(refPath, dbSNP, idxFile);
-		}
 	}
-////	use for test
-//	public VCFLocalLoader(String dbSNP) throws IOException {
-//		codec = new VCFCodec();
-//		input = dbSNP;
-//		seekableStream = new SeekableFileInputStream(input);
-//		readHeader();
-//	}
 	
 	public Iterator<PositionalVariantContext> iterator() throws IOException {
 		return collect();
@@ -80,6 +55,7 @@ public class VCFLocalLoader {
 	}
 	
 	private Iterator<PositionalVariantContext> collect() throws IOException {
+		@SuppressWarnings("resource")
 		AsciiLineReaderIterator iterator = new AsciiLineReaderIterator(
 				new AsciiLineReader(seekableStream));
 		ArrayList<PositionalVariantContext> variantContexts = new ArrayList<>();
@@ -139,7 +115,7 @@ public class VCFLocalLoader {
 	}
 	
 	public static String format(String inputFile) {
-		return inputFile + vcfIndex.INDEX_SUFFIX;
+		return inputFile + VcfIndex.INDEX_SUFFIX;
 	}
 	
 	public VCFHeader getHeader() {
@@ -164,20 +140,7 @@ public class VCFLocalLoader {
 			try {
 				seekableStream.close();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				throw new RuntimeException(e.toString());
 			}
 	}
-	
-//	public static void main(String[] args) throws IOException {
-//		VCFLocalLoader loader = new VCFLocalLoader("F:\\BGIBigData\\TestData\\VCF\\DNA1425995.vcf");
-//		Iterator<PositionalVariantContext> iterator = loader.iterator();
-//		while(iterator.hasNext()) {
-//			PositionalVariantContext pvc = iterator.next();
-//			if(pvc.getVariantContext() == null)
-//				System.err.println("pvc is null!");
-//			System.out.println(pvc.getPosition() + ":" + pvc.getVariantContext().getChr() + "\t" + pvc.getVariantContext().getStart());
-//		}
-//		loader.close();
-//	}
 }
