a399f632db481febd6d2371bfdc4ce746587da07
huangzhibo <huangzhibo@genomics.cn>
2016-12-13 09:29:16 +0800

    从hdfsLoader和localLoader中抽象出一个主类，localloader的调用方式没有改变，驻类驻类
    从hdfsLoader和localLoader中抽象出一个主类，方法调用一样

---
+++ src/main/java/org/bgi/flexlab/gaea/data/structure/vcf/AbstractVCFLoader.java
@@ -0,0 +1,158 @@
+package org.bgi.flexlab.gaea.data.structure.vcf;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.zip.GZIPInputStream;
+
+import htsjdk.tribble.FeatureCodecHeader;
+import htsjdk.tribble.TribbleException;
+import htsjdk.tribble.readers.AsciiLineReaderIterator;
+import htsjdk.tribble.readers.PositionalBufferedStream;
+import htsjdk.variant.variantcontext.VariantContext;
+import htsjdk.variant.vcf.VCFCodec;
+import htsjdk.variant.vcf.VCFHeader;
+
+public abstract class AbstractVCFLoader {
+	
+	public static final Set<String> BLOCK_COMPRESSED_EXTENSIONS = Collections.unmodifiableSet(new HashSet<String>(Arrays.asList(".gz", ".gzip", ".bgz", ".bgzf")));
+		
+	protected String input;
+			
+	protected AsciiLineReaderIterator iterator;
+	
+	protected long pos;
+
+	private VCFCodec codec;
+	
+	private FeatureCodecHeader header;
+		
+	public AbstractVCFLoader(String dbSNP) throws IOException {
+		codec = new VCFCodec();
+		input = dbSNP;
+		readHeader();
+		getSeekableStream();
+		seek(0);
+	}
+	
+	public abstract void getSeekableStream();
+	
+	public boolean hasNext() {
+		return iterator.hasNext();
+	}
+	
+	public PositionalVariantContext next() {
+		long pos = iterator.getPosition() + this.pos;
+		String line = iterator.next();
+		while(line.startsWith(VCFHeader.HEADER_INDICATOR)) {
+			pos = iterator.getPosition() + this.pos;
+			line = iterator.next();
+		}
+		VariantContext vc = codec.decode(line);
+		PositionalVariantContext pvc = new PositionalVariantContext(vc, pos);
+		return pvc;
+	}
+	
+	@Deprecated
+	public Iterator<PositionalVariantContext> iterator() throws IOException {
+		return collect();
+	}
+	
+	@Deprecated
+	public Iterator<PositionalVariantContext> iterator(long pos) throws IOException {
+		seek(pos);;
+		return collect();
+	}
+	
+	private Iterator<PositionalVariantContext> collect() throws IOException {
+		ArrayList<PositionalVariantContext> variantContexts = new ArrayList<>();
+		while(iterator.hasNext()) {
+			long pos = iterator.getPosition() + this.pos;
+			String line = iterator.next();
+			if(line.startsWith(VCFHeader.HEADER_INDICATOR))
+				continue;
+			VariantContext vc = codec.decode(line);
+			variantContexts.add(new PositionalVariantContext(vc, pos));
+		}
+		return variantContexts.iterator();
+	}
+	
+	public static class PositionalVariantContext {
+		
+		private VariantContext vc;
+		
+		private long position;
+		
+		public PositionalVariantContext(VariantContext vc, long position) {
+			this.vc = vc;
+			this.position = position;
+		}
+		
+		public VariantContext getVariantContext() {
+			return vc;
+		}
+		
+		public long getPosition() {
+			return position;
+		}
+	}
+	
+//	FIXME:function need to be fulfilled
+	public void query(String chr, int start, int end) {
+
+	}
+
+	private void readHeader() throws IOException {
+		InputStream is = null;
+        PositionalBufferedStream pbs = null;
+        try {
+            is = getInputStream(input);
+            if (hasBlockCompressedExtension(input)) {
+                // TODO -- warning I don't think this can work, the buffered input stream screws up position
+                is = new GZIPInputStream(new BufferedInputStream(is));
+            }
+            pbs = new PositionalBufferedStream(is);
+            header = codec.readHeader(codec.makeSourceFromStream(pbs));
+        } catch (Exception e) {
+            throw new TribbleException.MalformedFeatureFile("Unable to parse header with error: " + e.getMessage(), input, e);
+        } finally {
+            if (pbs != null) pbs.close();
+            else if (is != null) is.close();
+        }	
+	}
+	
+	public abstract InputStream getInputStream(String input);
+//	public static String format(String inputFile) {
+//		return inputFile + VcfIndex.INDEX_SUFFIX;
+//	}
+	
+	public VCFHeader getHeader() {
+		return (VCFHeader)(header.getHeaderValue());
+	}
+	
+	public abstract void seek(long pos);
+	
+	public static boolean hasBlockCompressedExtension(String fileName){
+		 for (final String extension : BLOCK_COMPRESSED_EXTENSIONS) {
+	            if (fileName.toLowerCase().endsWith(extension))
+	                return true;
+	     }
+	     return false;
+	}
+	
+	public void close() {
+		if(iterator != null)
+			try {
+				iterator.close();
+			} catch (IOException e) {
+				throw new RuntimeException(e.toString());
+			}
+	}
+	
+}
--- src/main/java/org/bgi/flexlab/gaea/data/structure/vcf/VCFLocalLoader.java
+++ src/main/java/org/bgi/flexlab/gaea/data/structure/vcf/VCFLocalLoader.java
@@ -1,166 +1,64 @@
 package org.bgi.flexlab.gaea.data.structure.vcf;
 
-import java.io.BufferedInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.zip.GZIPInputStream;
-
-//import org.bgi.flexlab.gaea.data.structure.reference.index.VcfIndex;
 import org.tukaani.xz.SeekableFileInputStream;
 
-import htsjdk.tribble.FeatureCodecHeader;
-import htsjdk.tribble.TribbleException;
 import htsjdk.tribble.readers.AsciiLineReader;
 import htsjdk.tribble.readers.AsciiLineReaderIterator;
-import htsjdk.tribble.readers.PositionalBufferedStream;
 import htsjdk.tribble.util.ParsingUtils;
-import htsjdk.variant.variantcontext.VariantContext;
-import htsjdk.variant.vcf.VCFCodec;
-import htsjdk.variant.vcf.VCFHeader;
 
-public class VCFLocalLoader {
-	
-	public static final Set<String> BLOCK_COMPRESSED_EXTENSIONS = Collections.unmodifiableSet(new HashSet<String>(Arrays.asList(".gz", ".gzip", ".bgz", ".bgzf")));
+public class VCFLocalLoader  extends AbstractVCFLoader {
+					
+	private SeekableFileInputStream seekableStream; 
 		
-	private String input;
-		
-	private SeekableFileInputStream seekableStream = null; 
-	
-	private AsciiLineReaderIterator iterator;
-	
-	private VCFCodec codec;
-	
-	private FeatureCodecHeader header;
-	
-	private long pos;
-	
 	public VCFLocalLoader(String dbSNP) throws IOException {
-		codec = new VCFCodec();
-		input = dbSNP;
-		readHeader();
-		seekableStream = new SeekableFileInputStream(input);
-		seek(0);
+		super(dbSNP);
 	}
 	
-	public boolean hasNext() {
-		return iterator.hasNext();
-	}
-	
-	public PositionalVariantContext next() {
-		long pos = iterator.getPosition() + this.pos;
-		String line = iterator.next();
-		while(line.startsWith(VCFHeader.HEADER_INDICATOR)) {
-			pos = iterator.getPosition() + this.pos;
-			line = iterator.next();
-		}
-		VariantContext vc = codec.decode(line);
-		PositionalVariantContext pvc = new PositionalVariantContext(vc, pos);
-		return pvc;
-	}
-	
-	@Deprecated
-	public Iterator<PositionalVariantContext> iterator() throws IOException {
-		return collect();
-	}
-	
-	@Deprecated
-	public Iterator<PositionalVariantContext> iterator(long pos) throws IOException {
-		seek(pos);;
-		return collect();
-	}
-	
-	private Iterator<PositionalVariantContext> collect() throws IOException {
-		ArrayList<PositionalVariantContext> variantContexts = new ArrayList<>();
-		while(iterator.hasNext()) {
-			long pos = iterator.getPosition() + this.pos;
-			String line = iterator.next();
-			if(line.startsWith(VCFHeader.HEADER_INDICATOR))
-				continue;
-			VariantContext vc = codec.decode(line);
-			variantContexts.add(new PositionalVariantContext(vc, pos));
-		}
-		return variantContexts.iterator();
-	}
-	
-	public class PositionalVariantContext {
-		
-		private VariantContext vc;
-		
-		private long position;
-		
-		public PositionalVariantContext(VariantContext vc, long position) {
-			this.vc = vc;
-			this.position = position;
-		}
-		
-		public VariantContext getVariantContext() {
-			return vc;
-		}
-		
-		public long getPosition() {
-			return position;
-		}
-	}
-	
-//	FIXME:function need to be fulfilled
-	public void query(String chr, int start, int end) {
-
-	}
-
-	private void readHeader() throws IOException {
-		InputStream is = null;
-        PositionalBufferedStream pbs = null;
-        try {
-            is = ParsingUtils.openInputStream(input);
-            if (hasBlockCompressedExtension(input)) {
-                // TODO -- warning I don't think this can work, the buffered input stream screws up position
-                is = new GZIPInputStream(new BufferedInputStream(is));
-            }
-            pbs = new PositionalBufferedStream(is);
-            header = codec.readHeader(codec.makeSourceFromStream(pbs));
-        } catch (Exception e) {
-            throw new TribbleException.MalformedFeatureFile("Unable to parse header with error: " + e.getMessage(), input, e);
-        } finally {
-            if (pbs != null) pbs.close();
-            else if (is != null) is.close();
-        }	
-	}
-	
-//	public static String format(String inputFile) {
-//		return inputFile + VcfIndex.INDEX_SUFFIX;
-//	}
-	
-	public VCFHeader getHeader() {
-		return (VCFHeader)(header.getHeaderValue());
-	}
-	
-	public void seek(long pos) throws IOException {
+	@Override
+	public void seek(long pos) {
 		this.pos = pos;
-		seekableStream.seek(this.pos);
+		try {
+			seekableStream.seek(this.pos);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException(e);
+		}
 		iterator = new AsciiLineReaderIterator(new AsciiLineReader(seekableStream));
 	}
-	
-	public static boolean hasBlockCompressedExtension(String fileName){
-		 for (final String extension : BLOCK_COMPRESSED_EXTENSIONS) {
-	            if (fileName.toLowerCase().endsWith(extension))
-	                return true;
-	     }
-	     return false;
+
+	@Override
+	public void getSeekableStream() {
+		// TODO Auto-generated method stub
+		try {
+			seekableStream = new SeekableFileInputStream(input);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public InputStream getInputStream(String input) {
+		// TODO Auto-generated method stub
+		try {
+			return ParsingUtils.openInputStream(input);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException(e);
+		}
 	}
 	
-	public void close() {
-		if(iterator != null)
-			try {
-				iterator.close();
-			} catch (IOException e) {
-				throw new RuntimeException(e.toString());
-			}
+	public static void main(String[] args) throws IOException {
+		VCFLocalLoader loader = new VCFLocalLoader("F:\\BGIBigData\\TestData\\VCF\\DNA1425995.vcf");
+		loader.seek(6287);
+		while(loader.hasNext()) {
+			PositionalVariantContext pvc = loader.next();
+			System.out.println(pvc.getPosition() + ":" +
+								pvc.getVariantContext().getChr() + "\t" +
+								pvc.getVariantContext().getStart());
+		}
 	}
-	
 }
