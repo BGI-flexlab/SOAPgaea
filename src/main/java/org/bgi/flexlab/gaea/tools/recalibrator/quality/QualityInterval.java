48dd5b4001bf88ac3ee3e2801fa1dc4fc859b9ba
lishengkang <lishengkang@genomics.cn>
2016-12-23 17:50:14 +0800

    bqsr

---
+++ src/main/java/org/bgi/flexlab/gaea/tools/recalibrator/quality/QualityInterval.java
@@ -0,0 +1,105 @@
+package org.bgi.flexlab.gaea.tools.recalibrator.quality;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.bgi.flexlab.gaea.util.QualityUtils;
+
+public class QualityInterval implements Comparable<QualityInterval> {
+	public static final Set<QualityInterval> MY_EMPTY_SET = Collections.emptySet();
+
+	public final int qStart, qEnd, fixQ, level;
+	public final long obervation, error;
+	public Set<QualityInterval> subIntervals;
+
+	public QualityInterval(int qs, int qe, int fq, int lv, long ob, long errs, Set<QualityInterval> sub) {
+		this.qStart = qs;
+		this.qEnd = qe;
+		this.fixQ = fq;
+		this.level = lv;
+		this.obervation = ob;
+		this.error = errs;
+		this.subIntervals = sub;
+	}
+
+	public QualityInterval(int qs, int lv, long ob, long errs) {
+		this(qs, qs, qs, lv, ob, errs, MY_EMPTY_SET);
+	}
+
+	public QualityInterval(int qs, int qe, int lv, long ob, long errs) {
+		this(qs, qe, -1, lv, ob, errs, MY_EMPTY_SET);
+	}
+
+	private boolean hasFixQuality() {
+		return fixQ != -1;
+	}
+
+	public double getErrorRate() {
+		if (hasFixQuality())
+			return QualityUtils.qualityToErrorProbility(fixQ);
+		else if (obervation == 0)
+			return 0.0;
+		return (error + 1) / ((double) (obervation + 1));
+	}
+
+	public byte getQual() {
+		if (!hasFixQuality())
+			return QualityUtils.probilityToQuality(1 - getErrorRate(), 0);
+		else
+			return (byte) (fixQ & 0xff);
+	}
+
+	public boolean canMerge(QualityInterval other) {
+		if ((this.qEnd + 1 != other.qStart) && (other.qEnd + 1 != this.qStart))
+			return false;
+
+		return true;
+	}
+
+	public QualityInterval merge(QualityInterval toMerge) {
+		final QualityInterval left = this.compareTo(toMerge) < 0 ? this : toMerge;
+		final QualityInterval right = this.compareTo(toMerge) < 0 ? toMerge : this;
+
+		final long obs = left.obervation + right.obervation;
+		final long err = left.error + right.error;
+
+		final int level = Math.max(left.level, right.level) + 1;
+		final Set<QualityInterval> subIntervals = new HashSet<QualityInterval>(Arrays.asList(left, right));
+		QualityInterval merged = new QualityInterval(left.qStart, right.qEnd, -1, level, obs, err, subIntervals);
+
+		return merged;
+	}
+
+	@Override
+	public int compareTo(QualityInterval other) {
+		return this.qStart - other.qStart;
+	}
+
+	public double getPenalty(int minQual) {
+		return calculatePenalty(getErrorRate(), minQual);
+	}
+
+	private double calculatePenalty(final double globalErrorRate, int minQual) {
+		if (globalErrorRate == 0.0)
+			return 0.0;
+
+		if (subIntervals.isEmpty()) {
+			if (this.qEnd <= minQual)
+				return 0;
+			else {
+				return (Math.abs(Math.log10(getErrorRate()) - Math.log10(globalErrorRate))) * obervation;
+			}
+		} else {
+			double sum = 0;
+			for (final QualityInterval interval : subIntervals)
+				sum += interval.calculatePenalty(globalErrorRate, minQual);
+			return sum;
+		}
+	}
+
+	public String toString() {
+		return "QQ:" + qStart + "-" + qEnd;
+	}
+}
---
+++ src/main/java/org/bgi/flexlab/gaea/tools/recalibrator/quality/QualityQuantizer.java
@@ -0,0 +1,83 @@
+package org.bgi.flexlab.gaea.tools.recalibrator.quality;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.TreeSet;
+
+import org.bgi.flexlab.gaea.data.exception.UserException;
+import org.bgi.flexlab.gaea.util.QualityUtils;
+
+public class QualityQuantizer {
+	private List<Long> observationNumbers = null;
+	private final int level;
+	private final int minimumQuality;
+
+	public QualityQuantizer(final List<Long> obser, final int level, final int minQuality) {
+		this.observationNumbers = obser;
+		this.level = level;
+		this.minimumQuality = minQuality;
+
+		if (Collections.min(obser) < 0)
+			throw new UserException("Quality score has negative values!");
+		if (level < 0)
+			throw new UserException("quality level must be >= 0!");
+		if (minQuality < 0)
+			throw new UserException("minimum quality must be >= 0!");
+	}
+
+	private TreeSet<QualityInterval> quantize() {
+		final TreeSet<QualityInterval> intervals = new TreeSet<QualityInterval>();
+		for (int qStart = 0; qStart < observationNumbers.size(); qStart++) {
+			final long nObs = observationNumbers.get(qStart);
+			final double errorRate = QualityUtils.qualityToErrorProbility((byte) qStart);
+			final double nErrors = nObs * errorRate;
+			final QualityInterval qi = new QualityInterval(qStart, 0, nObs, (int) Math.floor(nErrors));
+			intervals.add(qi);
+		}
+
+		while (intervals.size() > level) {
+			mergeLowestPenaltyIntervals(intervals);
+		}
+
+		return intervals;
+	}
+
+	private void mergeLowestPenaltyIntervals(final TreeSet<QualityInterval> intervals) {
+		final Iterator<QualityInterval> iter = intervals.iterator();
+		final Iterator<QualityInterval> iterator = intervals.iterator();
+		iterator.next();
+
+		QualityInterval minMerge = null;
+		while (iterator.hasNext()) {
+			final QualityInterval left = iter.next();
+			final QualityInterval right = iterator.next();
+			if (left.canMerge(right)) {
+				final QualityInterval merged = left.merge(right);
+				if (minMerge == null || (merged.getPenalty(minimumQuality) < minMerge.getPenalty(minimumQuality))) {
+					minMerge = merged;
+				}
+			}
+		}
+
+		intervals.removeAll(minMerge.subIntervals);
+		intervals.add(minMerge);
+	}
+
+	public List<Byte> getIntervals() {
+		TreeSet<QualityInterval> qualityIntervals = quantize();
+		final List<Byte> map = new ArrayList<Byte>(observationNumbers.size());
+		map.addAll(Collections.nCopies(observationNumbers.size(), Byte.MIN_VALUE));
+		for (final QualityInterval interval : qualityIntervals) {
+			for (int q = interval.qStart; q <= interval.qEnd; q++) {
+				map.set(q, interval.getQual());
+			}
+		}
+
+		if (Collections.min(map) == Byte.MIN_VALUE)
+			throw new UserException("quantized quality score map contains an un-initialized value!");
+
+		return map;
+	}
+}
