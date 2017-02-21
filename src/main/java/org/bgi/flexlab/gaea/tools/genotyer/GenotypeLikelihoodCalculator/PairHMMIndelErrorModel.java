27aa9544f1baa972dea2bb23e79edd931076901f
zym1905 <zym1905@gmail.com>
2017-02-21 14:07:12 +0800

    improve import
    improve import

--- src/main/java/org/bgi/flexlab/gaea/tools/genotyer/GenotypeLikelihoodCalculator/PairHMMIndelErrorModel.java
+++ src/main/java/org/bgi/flexlab/gaea/tools/genotyer/GenotypeLikelihoodCalculator/PairHMMIndelErrorModel.java
@@ -25,13 +25,6 @@
 
 package org.bgi.flexlab.gaea.tools.genotyer.GenotypeLikelihoodCalculator;
 
-import htsjdk.samtools.SAMRecord;
-
-import java.io.PrintStream;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import htsjdk.variant.variantcontext.Allele;
 import org.bgi.flexlab.gaea.data.exception.UserException;
 import org.bgi.flexlab.gaea.data.structure.alignment.AlignmentsBasic;
@@ -45,6 +38,10 @@ import org.bgi.flexlab.gaea.util.ReadUtils;
 import org.bgi.flexlab.gaea.util.pairhmm.ExactPairHMM;
 import org.bgi.flexlab.gaea.util.pairhmm.OriginalPairHMM;
 import org.bgi.flexlab.gaea.util.pairhmm.PairHMM;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class PairHMMIndelErrorModel {
     public static final int BASE_QUAL_THRESHOLD = 20;
