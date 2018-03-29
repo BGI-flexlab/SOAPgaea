842c0f4eb9f4628b811cf647961572680d6b08ef
huangzhibo <huangzhibo@genomics.cn>
2018-03-29 19:17:18 +0800

    add multi sort function: one sample per reducer

--- src/main/java/org/bgi/flexlab/gaea/tools/mapreduce/bamsort/BamSort.java
+++ src/main/java/org/bgi/flexlab/gaea/tools/mapreduce/bamsort/BamSort.java
@@ -19,14 +19,19 @@ package org.bgi.flexlab.gaea.tools.mapreduce.bamsort;
 import htsjdk.samtools.SAMFileHeader;
 import htsjdk.samtools.SAMReadGroupRecord;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.LazyOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.mapreduce.lib.partition.InputSampler;
 import org.apache.hadoop.mapreduce.lib.partition.TotalOrderPartitioner;
 import org.bgi.flexlab.gaea.data.mapreduce.input.header.SamHdfsFileHeader;
+import org.bgi.flexlab.gaea.data.mapreduce.output.bam.GaeaBamOutputFormat;
 import org.bgi.flexlab.gaea.data.mapreduce.writable.SamRecordWritable;
 import org.bgi.flexlab.gaea.framework.tools.mapreduce.BioJob;
 import org.bgi.flexlab.gaea.framework.tools.mapreduce.ToolsRunner;
@@ -40,6 +45,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class BamSort extends ToolsRunner {
 
@@ -47,23 +53,52 @@ public class BamSort extends ToolsRunner {
     final String intermediateOutName = "hadoopBamSort";
     private SAMFileHeader header;
     private Map<String, String> formatSampleName;
+    private List<String> sampleNames;
+    BioJob job;
+    Configuration conf;
+    List<Path> inputs;
+    Path tmpPath;
+    Path outputPath;
 
     public BamSort(){
         this.toolsDescription = "Gaea BamSort";
+        sampleNames = new ArrayList<>();
     }
 
 
     public int runSort(String[] args) throws IOException, ClassNotFoundException, InterruptedException, URISyntaxException {
-        BioJob job = BioJob.getInstance();
-        Configuration conf = job.getConfiguration();
+        job = BioJob.getInstance();
+        conf = job.getConfiguration();
         String[] remainArgs = remainArgs(args, conf);
         options = new BamSortOptions();
         options.parse(remainArgs);
-
-        Path tmpPath = new Path(options.getTmpPath());
-
+        tmpPath = new Path(options.getTmpPath());
+        outputPath = new Path(options.getOutdir());
         options.setHadoopConf(remainArgs, conf);
 
+        inputs = new ArrayList<>();
+        for (String in : options.getStrInputs())
+            inputs.add(new Path(in));
+        job.setHeader(inputs, outputPath);
+
+        header = SamHdfsFileHeader.getHeader(conf);
+        if(header == null){
+            System.err.println("Header is null!");
+            System.exit(-1);
+        }
+
+        for (SAMReadGroupRecord rg : header.getReadGroups()) {
+            if(!sampleNames.contains(rg.getSample()))
+                sampleNames.add(rg.getSample());
+        }
+
+        if(options.isMultiSample())
+            return runMultiSort();
+
+        return runSingleSort();
+    }
+
+    public int runSingleSort() throws IOException, ClassNotFoundException, InterruptedException, URISyntaxException {
         if (!options.isMultiSample())
             Utils.configureSampling(tmpPath, intermediateOutName, conf);
 
@@ -71,25 +106,13 @@ public class BamSort extends ToolsRunner {
         conf.set(SortOutputFormat.OUTPUT_NAME_PROP, intermediateOutName);
         conf.set(SortOutputFormat.OUTPUT_SAM_FORMAT_PROPERTY, options.getOutputFormat());
 
-        final List<Path> inputs = new ArrayList<>(options.getStrInputs().size());
-        for (final String in : options.getStrInputs())
-            inputs.add(new Path(in));
-
-        job.setHeader(inputs, new Path(options.getOutdir()));
-        header = SamHdfsFileHeader.getHeader(conf);
-        if(header == null){
-            System.err.println("Header is null!");
-            System.exit(-1);
-        }
-
         if (header.getReadGroups().size() == 1) {
             options.setMultiSample(false);
         }
-
         job.setJarByClass(BamSort.class);
         job.setMapperClass(SortMapper.class);
         job.setReducerClass(SortReducer.class);
-        job.setJobName("multi sample sort");
+        job.setJobName("bamsort");
 
         job.setMapOutputKeyClass(LongWritable.class);
         job.setOutputKeyClass(NullWritable.class);
@@ -127,9 +150,9 @@ public class BamSort extends ToolsRunner {
 
             job.setPartitionerClass(TotalOrderPartitioner.class);
             InputSampler.writePartitionFile(
-                            job,
-                            new InputSampler.RandomSampler<LongWritable, SamRecordWritable>(
-                                    0.01, 10000, Math.max(100, options.getReducerNum())));
+                    job,
+                    new InputSampler.RandomSampler<LongWritable, SamRecordWritable>(
+                            0.01, 10000, Math.max(100, options.getReducerNum())));
             System.out.printf("sort :: Sampling complete in %d.%03d s.\n",
                     t.stopS(), t.fms());
             String partitionFile = TotalOrderPartitioner.getPartitionFile(job.getConfiguration());
@@ -141,6 +164,60 @@ public class BamSort extends ToolsRunner {
 
         boolean success = job.waitForCompletion(true);
         return success ? 0 : 1;
+    }
+
+    public int runMultiSort() throws IOException, ClassNotFoundException, InterruptedException {
+        job.setJarByClass(BamSort.class);
+        job.setMapperClass(MultiSortMapper.class);
+        job.setReducerClass(MultiSortReducer.class);
+        job.setJobName("multi bamsort");
+        job.setNumReduceTasks(sampleNames.size());
+
+        job.setMapOutputKeyClass(PairWritable.class);
+        job.setOutputKeyClass(NullWritable.class);
+        job.setOutputValueClass(SamRecordWritable.class);
+
+        job.setAnySamInputFormat(options.getInputFormat());
+        LazyOutputFormat.setOutputFormatClass(job, GaeaBamOutputFormat.class);
+
+        for (Path in : inputs)
+            FileInputFormat.addInputPath(job, in);
+
+        FileOutputFormat.setOutputPath(job, tmpPath);
+
+        job.setPartitionerClass(FirstPartitioner.class);
+
+        FileSystem fs = tmpPath.getFileSystem(conf);
+        if(job.waitForCompletion(true)){
+            int loop = 0;
+            for (String fileName : sampleNames){
+                Path outputPart = getSampleOutputPath(fileName);
+                while (outputPart == null && loop < 10){
+                    TimeUnit.MILLISECONDS.sleep(6000);
+                    outputPart = getSampleOutputPath(fileName);
+                    loop ++;
+                }
+                Path outputName = new Path(options.getOutdir() + "/" + fileName+".bam");
+                fs.rename(outputPart, outputName);
+            }
+            fs.delete(tmpPath, true);
+            return 0;
+        }
+        return 1;
+    }
+
+    private Path getSampleOutputPath(String sample) throws IOException {
+        FileSystem fs = tmpPath.getFileSystem(conf);
+        FileStatus[] fileStatuses = fs.globStatus(new Path(options.getTmpPath() + "/" + sample + "-r-[0-9]*"));
+        if(fileStatuses.length == 0){
+            System.err.println(sample+": cann't get the output part file!");
+            FileStatus[] fss = fs.globStatus(new Path(options.getTmpPath() + "/*"));
+            for (FileStatus f: fss){
+                System.err.println("OutPath" + f.getPath().toString());
+            }
+            return null;
+        }
+        return fileStatuses[0].getPath();
     }
 
     @Override
---
+++ src/main/java/org/bgi/flexlab/gaea/tools/mapreduce/bamsort/MultiSortMapper.java
@@ -0,0 +1,77 @@
+/*******************************************************************************
+ * Copyright (c) 2017, BGI-Shenzhen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *******************************************************************************/
+package org.bgi.flexlab.gaea.tools.mapreduce.bamsort;
+
+import htsjdk.samtools.SAMFileHeader;
+import htsjdk.samtools.SAMRecord;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.bgi.flexlab.gaea.data.mapreduce.input.header.SamHdfsFileHeader;
+import org.bgi.flexlab.gaea.data.mapreduce.writable.SamRecordWritable;
+import org.bgi.flexlab.gaea.data.structure.bam.GaeaSamRecord;
+
+import java.io.IOException;
+
+public class MultiSortMapper extends
+		Mapper<LongWritable, SamRecordWritable, PairWritable, SamRecordWritable> {
+	private String type = "all";
+	private PairWritable resultKey;
+	private SAMFileHeader header;
+
+	@Override
+	protected void setup(Context context) throws IOException {
+		Configuration conf = context.getConfiguration();
+		BamSortOptions options = new BamSortOptions();
+		options.getOptionsFromHadoopConf(conf);
+		type = options.getType();
+		SAMFileHeader _header = SamHdfsFileHeader.getHeader(conf);;
+		options = new BamSortOptions();
+		options.getOptionsFromHadoopConf(conf);
+		if(options.getRenames() != null)
+			header = BamSortUtils.replaceSampleName(_header.clone(), options.getRenames());
+		else
+			header = _header;
+		resultKey = new PairWritable();
+	}
+
+	private boolean filter(GaeaSamRecord sam,Context context){
+		boolean result = false;
+		if(type.equals("all"))
+			result = true;
+		else if(type.toLowerCase().equals("unmap")){
+			if(sam.getReadUnmappedFlag()){
+				context.getCounter("statics", "unmapped read").increment(1);
+				result = true;
+			}
+		}
+		return result;
+	}
+
+	public void map(LongWritable key, SamRecordWritable value, Context context)
+			throws IOException, InterruptedException {
+		GaeaSamRecord sam = new GaeaSamRecord(header, value.get());
+		String sampleName = sam.getReadGroup().getSample();
+		String secondKey = String.format("%03d",sam.getReferenceIndex())
+				+ "-" + String.format("%09d",sam.getAlignmentStart())
+				+ "-" + String.format("%09d",sam.getAlignmentEnd());
+		resultKey.set(sampleName, secondKey);
+		if(filter(sam,context)){
+			context.write(resultKey, value);
+		}
+	}
+}
